bce49708eb6040cea70a96eb59bce6e46d80fa64
Milan Augustín <xaugus09@stud.fit.vutbr.cz>
2018-06-24 22:53:02 +0200

    fix

--- src/main/java/kotlik/chatbot/utils/StreamHandler.java
+++ src/main/java/kotlik/chatbot/utils/StreamHandler.java
@@ -39,7 +39,6 @@ public class StreamHandler {
                 switch (inputText) {
                     case "restart":
                         service.stop();
-                        service.restart();
                     case "start":
                         thread.join();
                         thread = new Thread(service);
